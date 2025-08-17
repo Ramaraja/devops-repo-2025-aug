b955b9023cdcb42682b37847f3da8f6d2dd36c0b
Ramaraja <ramaraja.rm@gmail.com>
2025-08-17 11:12:24 +0530

    Add files via upload

---
+++ sample-project-1/tests/test_app.py
@@ -0,0 +1,16 @@
+# Tests
+
+import pytest
+from app import add, subtract
+
+def test_add():
+    assert add(2, 3) == 5
+    assert add(-1, 1) == 0
+
+def test_subtract():
+    assert subtract(5, 3) == 2
+    assert subtract(0, 3) == -3
+
+# To demonstrate a failing test (optional)
+def test_fail():
+    assert subtract(2, 2) == 3  # This will fail
